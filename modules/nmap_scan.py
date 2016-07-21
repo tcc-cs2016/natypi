1cab97aca974038e9e6e4b12e03d48518204b6a3
tcc-cs2016 <natalia.meziat@yahoo.com>
2016-07-21 19:01:19 -0300

    Create nmap_scan.py

---
+++ modules/nmap_scan.py
@@ -0,0 +1,14 @@
+import subprocess
+import shlex
+
+def run(**args):
+    f_result = '/natypi/data/nmap_scan_result.txt'
+    command = 'nmap -T4 -A -v 192.168.0.1/24'
+
+    command = shlex.split(command)
+    process = subprocess.Popen(command, stdout=subprocess.PIPE)
+    output, err = process.communicate()
+    result  = '********** [ STDOUT ] **********\n%s********** [ STDERR ] **********\n%s' % (output, err)
+    with open(f_result, 'w') as f:
+        f.write(result)
+    return result
