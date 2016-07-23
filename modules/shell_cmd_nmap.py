56d1aa109d4c6cfc84eb968b57a955b0cdf1b200
tcc-cs2016 <natalia.meziat@yahoo.com>
2016-07-23 12:13:40 -0300

    Update shell_cmd_nmap.py

--- modules/shell_cmd_nmap.py
+++ modules/shell_cmd_nmap.py
@@ -2,10 +2,9 @@ import subprocess
 import shlex
 
 def run(**args):
-    f_result = '/natypi/data/nmap_scan_result.txt'
-    #command = 'nmap -T4 -A -v 192.168.0.1/24'
-    command = 'ping -c10 192.168.0.11'
-    
+    f_result = '../natypi/data/nmap_scan_result.txt'
+    command = 'nmap -T4 -A -v 192.168.0.1/24'
+
     command = shlex.split(command)
     process = subprocess.Popen(command, stdout=subprocess.PIPE)
     output, err = process.communicate()
